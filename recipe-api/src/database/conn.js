2f6744ea8c22f962029d23beaf91f8c974a01e3b
andy-johnson <jajohnson.dev@gmail.com>
2022-08-28 09:44:35 -0600

    adds recipe-api

---
+++ recipe-api/src/database/conn.js
@@ -0,0 +1,32 @@
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const connectionString = process.env.DB_URI 
+const connectionKey = process.env.DB_KEY
+
+console.log(connectionString)
+
+const client = new MongoClient(connectionString, {
+    sslKey: connectionKey,
+    sslCert: connectionKey,
+    serverApi: ServerApiVersion.v1
+})
+
+let dbConnection;
+
+module.exports = {
+    connectToServer: function(callback) {
+        client.connect(function (err, db) {
+            if (err || !db) {
+                return callback(err);
+            }
+        
+            dbConnection = db.db("Hunger")
+            console.log("Successfully connected to MongoDB.")
+
+            return callback();
+        });
+    },
+
+    getDb: function () {
+        return dbConnection;
+    }
+};
